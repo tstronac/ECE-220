228f3c0bc50c081ca5a4c18b655d229e2404c554
tstronac <tstronac@ualberta.ca>
2021-03-11 00:10:42 -0700

    FixedUp

---
+++ cmake-build-debug/CMakeFiles/ECE_220.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ECE_220.dir/prog1.c.obj"
+  "ECE_220.exe"
+  "ECE_220.exe.manifest"
+  "ECE_220.lib"
+  "ECE_220.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ECE_220.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ prog1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main() {
+    int arg2;
+    string arg1;
+    printf("Please enter the address and which bit of the address [0-7]");
+    scanf("%s" , arg1);
+    scanf("%i", arg2);
+    return 0;
+}
